d50b547909be793c1b6f889c8faab940425e0678
NikolaStamenkovski <118392578+NikolaStamenkovski@users.noreply.github.com>
2022-12-06 21:21:43 +0100

    Add files via upload

---
+++ Homework1 JS/script.js
@@ -0,0 +1,11 @@
+var phonePrice = 119.95;
+var phonePieces = 30; 
+var sum = phonePrice * phonePieces ;
+
+console.log(sum);
+
+var sum =3598.5
+var tax = 5 ;
+var  totalPrice =sum + sum/100*5
+console.log( totalPrice);
+
